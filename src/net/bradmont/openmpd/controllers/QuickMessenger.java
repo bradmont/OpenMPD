6beeb2a8ac8ab6cbae37b4409632061a500aeba8
Brad Stewart <s+github@bradstewart.ca>
2014-02-11 10:39:31 -0500

    Moved quick messages to a separate file

---
+++ src/net/bradmont/openmpd/controllers/QuickMessenger.java
@@ -0,0 +1,187 @@
+package net.bradmont.openmpd.controllers;
+
+import net.bradmont.openmpd.*;
+import net.bradmont.openmpd.models.*;
+import net.bradmont.openmpd.views.ContactDetail;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+
+import android.net.Uri;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import com.fima.cardsui.objects.Card;
+
+/*
+ * Manage Quick Action messages, including selecting and sending messages,
+ * adding and editing message templates.
+ */
+public class QuickMessenger {
+
+    protected Contact contact;
+    protected String messageFilter = "";
+
+	public QuickMessenger(Contact contact, String messageFilter){
+        this.contact=contact;
+        this.messageFilter = messageFilter;
+    }
+    public void showQuickMessageDialog(){
+
+        String sql = "select _id, name, subject, body from quick_message where notification_type = '" + messageFilter+ "' order by name;";
+        AlertDialog.Builder ad = new AlertDialog.Builder(OpenMPD.getInstance());
+        ad.setTitle(R.string.choose_a_template);
+
+        ad.setPositiveButton(R.string.send_blank, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                sendMessage(null, "", "");
+            }
+        });
+        ad.setNegativeButton(R.string.new_template, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                showEditTemplateDialog(null);
+            }
+        });
+
+        // set up listview
+        ListView message_list = (ListView) ((LayoutInflater) OpenMPD.getInstance()
+            .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+            .inflate(R.layout.list, null);
+        Cursor cur = MPDDBHelper.get().getReadableDatabase().rawQuery(sql, null);
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(OpenMPD.getInstance(),
+            R.layout.quick_message_list_item, cur,
+            new String [] {"name"},
+            new int [] {R.id.name});
+        message_list.setAdapter(adapter);
+        final AlertDialog dialog = ad.create();
+
+        message_list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                int position, long id) {
+                QuickMessage message = new QuickMessage((int)id);
+                showPreview(message, dialog);
+            }
+        });
+
+        dialog.setView(message_list);
+        dialog.show();
+    }
+
+    public void showPreview(final QuickMessage message, final Dialog parentDialog){
+        // show a preview of the message, and the option to show it
+        AlertDialog.Builder builder = new AlertDialog.Builder(OpenMPD.getInstance());
+
+        final String body = message.getString("body").replace("$name", contact.getString("fname"));
+        final String subject = message.getString("subject").replace("$name", contact.getString("fname"));
+
+        builder.setMessage(body)
+                .setTitle(message.getString("subject"));
+
+        builder.setPositiveButton(R.string.send, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                sendMessage(message, subject, body);
+                parentDialog.dismiss();
+            }
+        });
+        builder.setNeutralButton(R.string.edit_template, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) { 
+                showEditTemplateDialog(message);
+                parentDialog.dismiss();
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) { }
+        });
+
+        builder.show();
+
+    }
+    private void showEditTemplateDialog(final QuickMessage message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(OpenMPD.getInstance());
+        builder.setTitle(R.string.edit_email_template);
+        final LinearLayout layout = (LinearLayout) ((LayoutInflater) OpenMPD.getInstance()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
+                .inflate(R.layout.quick_message_edit, null);
+        builder.setView(layout);
+        if (message != null){
+            ((EditText) layout.findViewById(R.id.name))
+                .setText(message.getString("name"));
+            ((EditText) layout.findViewById(R.id.email_subject))
+                .setText(message.getString("subject"));
+            ((EditText) layout.findViewById(R.id.email_body))
+                .setText(message.getString("body"));
+        }
+        builder.setPositiveButton(R.string.save_and_send, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                saveTemplate(message, layout);
+                String body = message.getString("body").replace("$name", contact.getString("fname"));
+                String subject = message.getString("subject").replace("$name", contact.getString("fname"));
+                sendMessage(message, subject, body);
+            }
+        });
+        builder.setNeutralButton(R.string.save, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                saveTemplate(message, layout);
+                OpenMPD.getInstance().userMessage(R.string.saved);
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) { }
+        });
+        builder.show();
+    }
+    private void saveTemplate(QuickMessage message, LinearLayout layout){
+        if (message == null){
+            message = new QuickMessage();
+            message.setValue("notification_type", messageFilter);
+        }
+        message.setValue("name", 
+            ((EditText) layout.findViewById(R.id.name)).getText().toString());
+        message.setValue("subject", 
+            ((EditText) layout.findViewById(R.id.email_subject)).getText().toString());
+        message.setValue("body", 
+            ((EditText) layout.findViewById(R.id.email_body)).getText().toString());
+        message.setValue("customized", true);
+        message.dirtySave();
+    }
+
+    private void sendMessage(QuickMessage message, String subject, String body){
+        String recipient_address = "";
+        try {
+            EmailAddress email = (EmailAddress) MPDDBHelper
+                .getModelByField("email_address", "contact_id", contact.getInt("id"));
+            recipient_address = email.getString("address");
+        } catch (Exception e){
+            Log.i("net.bradmont.openmpd", "Problem getting email address");
+        }
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String [] {recipient_address});
+        if (message != null){
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            intent.putExtra(Intent.EXTRA_TEXT, body);
+        }
+
+        OpenMPD.getInstance()
+            .startActivity(
+                Intent.createChooser(intent, "Send Email"));
+    }
+
+	
+}
--- src/net/bradmont/openmpd/views/cards/NotificationCard.java
+++ src/net/bradmont/openmpd/views/cards/NotificationCard.java
@@ -1,6 +1,7 @@
 package net.bradmont.openmpd.views.cards;
 
 import net.bradmont.openmpd.*;
+import net.bradmont.openmpd.controllers.QuickMessenger;
 import net.bradmont.openmpd.models.*;
 import net.bradmont.openmpd.views.ContactDetail;
 
@@ -93,151 +94,10 @@ public abstract class NotificationCard extends Card implements View.OnClickListe
 
                 break;
             case R.id.button_quick_email:
-                showQuickMessageDialog();
+                QuickMessenger q = new QuickMessenger(contact, getNotificationType());
+                q.showQuickMessageDialog();
                 break;
         }
-    }
-    public void showQuickMessageDialog(){
-
-        String sql = "select _id, name, subject, body from quick_message where notification_type = '" + getNotificationType() + "' order by name;";
-        AlertDialog.Builder ad = new AlertDialog.Builder(OpenMPD.getInstance());
-        ad.setTitle(R.string.choose_a_template);
-
-        ad.setPositiveButton(R.string.send_blank, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                sendMessage(null, "", "");
-            }
-        });
-        ad.setNegativeButton(R.string.new_template, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                showEditTemplateDialog(null);
-            }
-        });
-
-        // set up listview
-        ListView message_list = (ListView) ((LayoutInflater) OpenMPD.getInstance()
-            .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-            .inflate(R.layout.list, null);
-        Cursor cur = MPDDBHelper.get().getReadableDatabase().rawQuery(sql, null);
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(OpenMPD.getInstance(),
-            R.layout.quick_message_list_item, cur,
-            new String [] {"name"},
-            new int [] {R.id.name});
-        message_list.setAdapter(adapter);
-        final AlertDialog dialog = ad.create();
-
-        message_list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, final View view,
-                int position, long id) {
-                QuickMessage message = new QuickMessage((int)id);
-                showPreview(message, dialog);
-            }
-        });
-
-        dialog.setView(message_list);
-        dialog.show();
-    }
-
-    public void showPreview(final QuickMessage message, final Dialog parentDialog){
-        // show a preview of the message, and the option to show it
-        AlertDialog.Builder builder = new AlertDialog.Builder(OpenMPD.getInstance());
-
-        final String body = message.getString("body").replace("$name", contact.getString("fname"));
-        final String subject = message.getString("subject").replace("$name", contact.getString("fname"));
-
-        builder.setMessage(body)
-                .setTitle(message.getString("subject"));
-
-        builder.setPositiveButton(R.string.send, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                sendMessage(message, subject, body);
-                parentDialog.dismiss();
-            }
-        });
-        builder.setNeutralButton(R.string.edit_template, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) { 
-                showEditTemplateDialog(message);
-                parentDialog.dismiss();
-            }
-        });
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) { }
-        });
-
-        builder.show();
-
-    }
-    private void showEditTemplateDialog(final QuickMessage message){
-        AlertDialog.Builder builder = new AlertDialog.Builder(OpenMPD.getInstance());
-        builder.setTitle(R.string.edit_email_template);
-        final LinearLayout layout = (LinearLayout) ((LayoutInflater) OpenMPD.getInstance()
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE))
-                .inflate(R.layout.quick_message_edit, null);
-        builder.setView(layout);
-        if (message != null){
-            ((EditText) layout.findViewById(R.id.name))
-                .setText(message.getString("name"));
-            ((EditText) layout.findViewById(R.id.email_subject))
-                .setText(message.getString("subject"));
-            ((EditText) layout.findViewById(R.id.email_body))
-                .setText(message.getString("body"));
-        }
-        builder.setPositiveButton(R.string.save_and_send, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                saveMessage(message, layout);
-                String body = message.getString("body").replace("$name", contact.getString("fname"));
-                String subject = message.getString("subject").replace("$name", contact.getString("fname"));
-                sendMessage(message, subject, body);
-            }
-        });
-        builder.setNeutralButton(R.string.save, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                saveMessage(message, layout);
-                OpenMPD.getInstance().userMessage(R.string.saved);
-            }
-        });
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) { }
-        });
-        builder.show();
-    }
-    private void saveMessage(QuickMessage message, LinearLayout layout){
-        if (message == null){
-            message = new QuickMessage();
-            message.setValue("notification_type", getNotificationType());
-        }
-        message.setValue("name", 
-            ((EditText) layout.findViewById(R.id.name)).getText().toString());
-        message.setValue("subject", 
-            ((EditText) layout.findViewById(R.id.email_subject)).getText().toString());
-        message.setValue("body", 
-            ((EditText) layout.findViewById(R.id.email_body)).getText().toString());
-        message.setValue("customized", true);
-        message.dirtySave();
-    }
-
-    private void sendMessage(QuickMessage message, String subject, String body){
-        String recipient_address = "";
-        try {
-            EmailAddress email = (EmailAddress) MPDDBHelper
-                .getModelByField("email_address", "contact_id", contact.getInt("id"));
-            recipient_address = email.getString("address");
-        } catch (Exception e){
-            Log.i("net.bradmont.openmpd", "Problem getting email address");
-        }
-
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_EMAIL, new String [] {recipient_address});
-        if (message != null){
-            intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-            intent.putExtra(Intent.EXTRA_TEXT, body);
-        }
-
-        OpenMPD.getInstance()
-            .startActivity(
-                Intent.createChooser(intent, "Send Email"));
     }
 
     public abstract String getNotificationType();
