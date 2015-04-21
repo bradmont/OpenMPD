805e4f3e83c7a3df28be08eb54237ded14d48457
Brad Stewart <s+github@bradstewart.ca>
2015-04-21 18:10:52 -0400

    Cleaned up some references

--- src/net/bradmont/openmpd/OnBootScheduler.java
+++ src/net/bradmont/openmpd/OnBootScheduler.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 
 import java.util.Calendar;
@@ -20,19 +21,16 @@ import net.bradmont.supergreen.models.ModelList;
  * Handles the device boot system broadcast, and schedules data updates
  */
 public class OnBootScheduler extends BroadcastReceiver {
-    private boolean instantiatedDB = false;
     public void onReceive(Context context, Intent intent) {
-        MPDDBHelper helper = MPDDBHelper.rawGet();
-        if (helper == null){
-            helper = new MPDDBHelper(context);
-            instantiatedDB = true;
-        }
+        Log.d("net.bradmont.openmpd", "received boot broadcast");
+        MPDDBHelper helper = MPDDBHelper.get();
         // set up our alarms for automatic background updating
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Calendar cal = new GregorianCalendar();
 
         ModelList accounts = helper.getReferenceModel("service_account").getAll();
         if  (accounts.size() == 0){
+            Log.d("net.bradmont.openmpd", "no accounts, not setting alarm");
             return;
         }
         int [] account_ids = new int [accounts.size()];
@@ -44,6 +42,6 @@ public class OnBootScheduler extends BroadcastReceiver {
             PendingIntent.getService(context, 0, new Intent(context, TntImportService.class).putExtra("net.bradmont.openmpd.account_ids", account_ids),
             PendingIntent.FLAG_UPDATE_CURRENT)
             );
-        helper.close();
+        Log.d("net.bradmont.openmpd", "alarmmanager set");
     }
 }
